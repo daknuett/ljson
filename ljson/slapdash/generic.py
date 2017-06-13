43b307176731f3e57592e8397d152027b42d7d92
Daniel Knuettel <daknuett@gmail.com>
2017-06-13 19:48:48 +0200

    initial slapdash implementation (no disk)

---
+++ ljson/slapdash/generic.py
@@ -0,0 +1,119 @@
+"""
+Some generic functions and classes for ljson.slapdash.
+
+"""
+
+from ..base.generic import datatypes, datatype_by_name, python_datatype_by_name
+import json
+
+class SlapdashHeader(object):
+	"""
+	A Header for ljson slapdash tables.
+	Used to describe the table (as good as possible).
+	"""
+	def __init__(self, descriptor):
+		self.descriptor = descriptor
+		if("__type__" in self.descriptor):
+			del(self.descriptor["__type__"])
+	def __repr__(self):
+		return "{mymod}.{myname}({descriptor})".format(mymod = type(self).__module__,
+				myname = type(self).__name__,
+				descriptor = self.descriptor)
+	def get_header(self):
+		"""
+		Return a JSON header.
+		"""
+		header = self.descriptor
+		header["__type__"] = "slapdash-header"
+
+		return json.dumps(header)
+
+	@staticmethod
+	def from_file(fin):
+		"""
+		Construct the header from the file.
+		"""
+		line = fin.readline()
+		data = json.loads(line)
+		if(not "__type__" in data or data["__type__"] != "slapdash-header"):
+			return SlapdashHeader({})
+		del(data["__type__"])
+		return Header(data)
+
+class SlapdashTable(object):
+	"""
+	A messy table.
+
+	SlapdashTable is used to store inhomogenous
+	data, like in a document-oriented database.
+	"""
+	def __getitem__(self, dct):
+		pass
+	def __delitem__(self, dct):
+		pass
+	def __next__(self):
+		pass
+	def __iter__(self):
+		pass
+	def __contains__(self, dct):
+		pass
+	def calculate_stats(self):
+		"""
+		Calculate statistical information about the 
+		data structure.
+		Returns a dict::
+
+			{
+				"length": int,
+				"field_count": dict,
+				"total_datatype_count": dict,
+				"per_field_datatype_count": dict
+			}
+		"""
+		pass
+	def insert_stats(self):
+		"""
+		Calculate statistical information about the 
+		data structure and store the gathered information
+		in the Header.
+		"""
+		pass
+
+	@staticmethod
+	def from_file(self, fin):
+		"""
+		Read a SlapdashTable from a file
+		"""
+		pass
+	def split(self, ignore = []):
+		"""
+		Split the table into tables containing exactly one document
+		type.
+
+		**Warning**: If the table highly inhomogenous, this will
+		produce a high number of tables.
+
+		Attribute names listed in ``ignore`` will be ignored by the split
+		process and the data will not appear in the resulting tables.
+		"""
+		pass
+	def additem(self, document):
+		"""
+		Add a document to the table.
+		"""
+		pass
+	def save(self, fout):
+		pass
+
+def document_matches(document, dct):
+	"""
+	Return True, if the document matches the
+	given dict. If a value in the dict is not contained 
+	in the document, this will return false.
+	"""
+	for k, v in dct.items():
+		if(k in document and document[k] == v):
+			continue
+		else:
+			return False
+	return True
---
+++ ljson/slapdash/mem.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+from .generic import SlapdashHeader, SlapdashTable, document_matches
+from ..base.generic import inversed_datatypes
+from collections import defaultdict
+
+
+
+
+class Table(SlapdashTable):
+	def __init__(self, header, documents):
+		self.header = header
+		self.documents = documents
+
+		self.document_count = len(documents)
+		self.insert_stats()
+		self._index = 0
+
+	def calculate_stats(self):
+		counter = defaultdict(int)
+		dtype_counter = defaultdict(int)
+		dtype_per_field_counter = defaultdict()
+
+		for doc in self.documents:
+			for k, v in doc.items():
+				counter[k] += 1
+				dtype_counter[inversed_datatypes[type(v)]] += 1
+				if(not k in dtype_per_field_counter):
+					dtype_per_field_counter[k] = defaultdict(int)
+				dtype_per_field_counter[k][inversed_datatypes[type(v)]] += 1
+
+		return {"length": self.document_count, "field_count": dict(counter),
+			"total_datatype_count": dict(dtype_counter),
+			"per_field_datatype_count": dict(dtype_per_field_counter)}
+
+	def insert_stats(self):
+		self.header.descriptor.update(self.calculate_stats())
+
+	def additem(self, document):
+		old_data = {k: self.header.descriptor[k] 
+			for k in ("field_count", "total_datatype_count", "per_field_datatype_count")}
+		
+		counter = defaultdict(int)
+		dtype_counter = defaultdict(int)
+		dtype_per_field_counter = defaultdict()
+
+		counter.update(old_data["field_count"])
+		dtype_counter.update(old_data["total_datatype_count"])
+		dtype_per_field_counter.update(old_data["per_field_datatype_count"])
+
+		raise_key_error = False
+		wrong_type = None
+
+		for k, v in document.items():
+			counter[k] += 1
+			try:
+				dtype_counter[inversed_datatypes[type(v)]] += 1
+				if(not k in dtype_per_field_counter):
+					dtype_per_field_counter[k] = defaultdict(int)
+				dtype_per_field_counter[k][inversed_datatypes[type(v)]] += 1
+			except KeyError as e:
+				raise_key_error = True
+				wrong_type = (k, v)
+				
+		if(raise_key_error):
+			raise KeyError("Unknown datatype for field {}: {}".format(*wrong_type))
+
+		self.document_count += 1
+				
+		self.header.descriptor.update({"length": self.document_count, "field_count": dict(counter),
+			"total_datatype_count": dict(dtype_counter),
+			"per_field_datatype_count": dict(dtype_per_field_counter)})
+
+		self.documents.append(document)
+				
+
+	def __delitem__(self, dct):
+		que = collections.deque()
+		for i, document in enumerate(self.documents):
+			if(document_matches(document, dct)):
+				que.appendleft(i)
+		if(not len(que)):
+			raise KeyError("no matching documents found: {}".format(dct))
+		for i in que:
+			del(self.documents[i])
+
+	def __next__(self):
+		if(self._index >= self.document_count):
+			self._index = 0
+			raise StopIteration()
+
+		res = self.documents[self._index]
+		self._index += 1
+		return res
+
+	def __contains__(self, dct):
+		for document in self.documents:
+			if(document_matches(document, dct)):
+				return True
+		return False
+	def __enter__(self):
+		return self
+	def __exit__(self, exc_type, exc_value, traceback):
+		return False
+
+	def __repr__(self):
+		return "{mymod}.{mytype}({header}, {table})".format(mytype = type(self).__name__, 
+				mymod = type(self).__module__,
+				header = repr(self.header),
+				descriptor = self.header.descriptor, 
+				table = self.documents)
+	def save(self, fout):
+		fout.write(self.header.get_header())
+		for r in self.documents:
+			fout.write("\n")
+			json.dump(r, fout)
+	
+	def __getitem__(self, dct):
+		return Selector(self.header, dct, self)
+	def __iter__(self):
+		return iter(self.documents)
+
+class Selector(object):
+	def __init__(self, header, dct, table):
+		self.header = header
+		self.documents = [r for r in table.documents if document_matches(r, dct)]
+		self.dct = dct
+		self.table = table
+		self._index = 0
+
+	def getone(self, column = None):
+		if(not len(self.documents)):
+			return None
+		if(column != None):
+			return self.documents[0][column]
+		else:
+			return self.documents[0]
+	def __getitem__(self, column):
+		return [document[column] for document in self.documents]
+	def __setitem__(self, column, value):
+		documents = self.table.documents
+		for i, r in enumerate(self.table.documents):
+			if(document_matches(r, self.dct)):
+				r[column] = value
+		self.table.documents = documents
+	def __next__(self):
+		if(self._index >= len(self.documents)):
+			self._index = 0
+			raise StopIteration()
+		res = self.documents[self._index]
+		self._index += 1
+		return res
+	def __iter__(self):
+		return iter(self.documents)
+	def __list__(self):
+		return list(self.documents)
+
