23346e50b4fb419be7e17ac89a4d4cae6ce9468c
Daniel Knuettel <daknuett@gmail.com>
2017-09-07 09:59:07 +0200

    added benchmarks

--- test/test_ljson_disk.py
+++ test/test_ljson_disk.py
@@ -1,7 +1,7 @@
 import ljson.base.mem
 import ljson.base.disk
 import ljson.base.generic
-import copy, os
+import copy, os, pytest
 
 from .data import data, header_descriptor, item_meg
 
@@ -260,3 +260,18 @@ def test_delete(tmpdir):
 	del(table[{"name": "peter"}])
 
 	assert list(table) == data_
+
+
+@pytest.mark.slow
+def test_speedup_delete(tmpdir, benchmark):
+	benchmark(test_delete, tmpdir)
+@pytest.mark.slow
+def test_speedup_edit(tmpdir, benchmark):
+	benchmark(test_edit, tmpdir)
+
+@pytest.mark.slow
+def test_speedup_contains(tmpdir, benchmark):
+	benchmark(test_contains)
+@pytest.mark.slow
+def test_speedup_selection_iter(tmpdir, benchmark):
+	benchmark(test_selection_iter, tmpdir)
